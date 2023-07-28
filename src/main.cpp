77c8b9eb59a41abbe434f957b113340fa3538b7a
Alexander Metzger <alexander@metzger.dk>
2023-07-28 13:54:16 -0700

    added langitude and longitude

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ void loop()
 {
     DHT_Result dhtResult = DHT_loop();
     MQ135_Result mqResult = MQ135_loop(dhtResult.temperature, dhtResult.humidity);
-    String dataString = "\"temperature\": " + String(dhtResult.temperature) + ", \"humidity\": " + String(dhtResult.humidity) + ", \"rzero\": " + String(mqResult.rzero) + ", \"correctedRZero\": " + String(mqResult.correctedRZero) + ", \"resistance\": " + String(mqResult.resistance) + ", \"ppm\": " + String(mqResult.ppm) + ", \"correctedPPM\": " + String(mqResult.correctedPPM);
+    String dataString = "\"device_id\": \"" + String(IOT_CONFIG_DEVICE_ID) + "\", \"latitude\": " + String(LATITUDE) + ", \"longitude\": " + String(LONGITUDE) + ", \"temperature\": " + String(dhtResult.temperature) + ", \"humidity\": " + String(dhtResult.humidity) + ", \"rzero\": " + String(mqResult.rzero) + ", \"correctedRZero\": " + String(mqResult.correctedRZero) + ", \"resistance\": " + String(mqResult.resistance) + ", \"ppm\": " + String(mqResult.ppm) + ", \"correctedPPM\": " + String(mqResult.correctedPPM);
     char* char_array = new char[dataString.length() + 1]; // +1 for null terminator
     strcpy(char_array, dataString.c_str());
     azure_loop(char_array);
